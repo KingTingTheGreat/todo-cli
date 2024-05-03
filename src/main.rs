d1e151ec4c36037aa7c5c3189fffb5ed24437564
Jeffrey Ting <jting@bu.edu>
2024-05-03 15:14:30 -0400

    testing previous two commits

--- src/main.rs
+++ src/main.rs
@@ -3,88 +3,112 @@ mod entry;
 mod status;
 
 use date::Date;
-use entry::Entry;
+use entry::*;
 use status::Status;
-// use colored::*;
 // use std::io;
 
 fn main() {
-    let date1 = Date::new(2024, 5, 2).unwrap();
-    let date2 = Date::new(2024, 5, 3).unwrap();
-    let date3 = Date::new(2023, 12, 31).unwrap();
-    println!("Date 1 == Date 2: {}", date1 == date2); // true
-    println!("Date 1 != Date 3: {}", date1 != date3); // true
-
-    println!("Date 1 < Date 3: {}", date1 < date3); // false
-    println!("Date 1 > Date 3: {}", date1 > date3); // true
-    let _entry = Entry::new(
-        "my entry".to_string(),
-        "my desc".to_string(),
-        date3,
-        "my category".to_string(),
+    let my_entry = Entry::new(
+        "Discussion Board".to_string(),
+        "last discussion board post".to_string(),
+        // Date::new(2024, 5, 5).unwrap(),
+        Date::now(),
+        "PH300".to_string(),
         Status::Done,
-        0,
+        1,
         0.0,
     );
 
-    println!("{}", date1);
+    let one_week = my_entry.get_due_date().add_days(7);
+    let entry1 = Entry::new(
+        "Final Paper".to_string(),
+        "final paper".to_string(),
+        // Date::new(2024, 5, 5).unwrap(),
+        one_week,
+        "PH300".to_string(),
+        Status::InProgress,
+        1,
+        0.0,
+    );
 
-    let mut dates = vec![
-        Date::new(2024, 5, 2).unwrap(),
-        Date::new(2023, 12, 31).unwrap(),
-        Date::new(2025, 1, 15).unwrap(),
-        Date::new(2024, 10, 8).unwrap(),
-    ];
+    let two_weeks = my_entry.get_due_date().add_days(14);
+    let entry2 = Entry::new(
+        "Final Paper".to_string(),
+        "last discussion board post".to_string(),
+        // Date::new(2024, 5, 5).unwrap(),
+        two_weeks,
+        "WS335".to_string(),
+        Status::InProgress,
+        1,
+        0.0,
+    );
 
-    println!("Before sorting: {:?}", dates);
+    let four_weeks = my_entry.get_due_date().add_days(28);
+    let entry3 = Entry::new(
+        "Final Exam".to_string(),
+        "last discussion board post".to_string(),
+        // Date::new(2024, 5, 5).unwrap(),
+        four_weeks,
+        "CS350".to_string(),
+        Status::InProgress,
+        1,
+        0.0,
+    );
 
-    dates.sort(); // Sort the dates
+    let one_month = my_entry.get_due_date().add_days(30);
+    let entry4 = Entry::new(
+        "Onboarding".to_string(),
+        "last discussion board ".to_string(),
+        // Date::new(2024, 5, 5).unwrap(),
+        one_month,
+        "Cargill".to_string(),
+        Status::NotStarted,
+        1,
+        0.0,
+    );
 
-    println!("After sorting: {:?}", dates);
+    let entry_past = Entry::new(
+        "OSN".to_string(),
+        "last discussion board post".to_string(),
+        // Date::new(2024, 5, 5).unwrap(),
+        Date::new(2020, 5, 5).unwrap(),
+        "Cargill".to_string(),
+        Status::NotStarted,
+        1,
+        0.0,
+    );
 
-    // Trying to create an invalid date
-    let invalid_date = Date::new(2024, 13, 32);
-    match invalid_date {
-        Ok(_) => println!("Invalid date: This should not be reached"),
-        Err(err) => println!("Error: {}", err), // Prints: "Error: Invalid date"
+    // print all entries
+    let mut entries = vec![my_entry, entry4, entry1, entry2, entry3, entry_past];
+    for entry in entries.iter() {
+        println!("{}", entry);
     }
 
-    let custom_now = Date::now();
-    println!("custom now: {}", custom_now);
+    // sort by due date
+    sort_by_due_date(&mut entries);
+    println!("\nSorted by due date:");
+    for entry in entries.iter() {
+        println!("{}", entry);
+    }
 
-    let mut entries = vec![
-        Entry::new(
-            "entry 1".to_string(),
-            "desc 1".to_string(),
-            Date::new(2023, 12, 31).unwrap(),
-            "category 1".to_string(),
-            Status::Done,
-            0,
-            0.0,
-        ),
-        Entry::new(
-            "entry 2".to_string(),
-            "desc 2".to_string(),
-            Date::new(2024, 10, 8).unwrap(),
-            "category 2".to_string(),
-            Status::InProgress,
-            1,
-            1.0,
-        ),
-        Entry::new(
-            "entry 3".to_string(),
-            "desc 3".to_string(),
-            Date::new(2025, 1, 15).unwrap(),
-            "category 3".to_string(),
-            Status::NotStarted,
-            2,
-            2.0,
-        ),
-    ];
+    // sort by name
+    sort_by_name(&mut entries);
+    println!("\nSorted by name:");
+    for entry in entries.iter() {
+        println!("{}", entry);
+    }
 
-    entry::sort(&mut entries, entry::SortType::Name);
-    println!("Entries sorted by name: {:#?}", entries);
+    // sort by status
+    sort_by_status(&mut entries);
+    println!("\nSorted by status:");
+    for entry in entries.iter() {
+        println!("{}", entry);
+    }
 
-    entry::sort(&mut entries, entry::SortType::Status);
-    println!("Entries sorted by due date: {:#?}", entries);
+    // sort by category
+    sort_by_category(&mut entries);
+    println!("\nSorted by category:");
+    for entry in entries.iter() {
+        println!("{}", entry);
+    }
 }
