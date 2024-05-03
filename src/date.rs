513f5925ca54ee121698c1aeb569ee405e836de2
Jeffrey Ting <jting@bu.edu>
2024-05-03 15:14:07 -0400

    getters. get date n days from this one

--- src/date.rs
+++ src/date.rs
@@ -1,16 +1,16 @@
+use chrono::{Datelike, Local};
+use serde::{Deserialize, Serialize};
 use std::cmp::Ordering;
 use std::fmt;
-use chrono::{Local, Datelike};
-use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Date {
-    year: u32,
-    month: u32,
-    day: u32
+    year: u16,
+    month: u8,
+    day: u8,
 }
 
-fn is_leap(year: u32) -> bool {
+fn is_leap(year: u16) -> bool {
     if year % 4 != 0 {
         return false;
     }
@@ -20,17 +20,36 @@ fn is_leap(year: u32) -> bool {
     return year % 400 == 0;
 }
 
+fn num_days(year: u16, month: u8) -> u8 {
+    let month_days: [u8; 12] = [
+        31,
+        if is_leap(year) { 29 } else { 28 },
+        31,
+        30,
+        31,
+        30,
+        31,
+        31,
+        30,
+        31,
+        30,
+        31,
+    ];
+
+    return month_days[month as usize - 1];
+}
+const MONTH_DAYS: [u8; 12] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 impl Date {
-    pub fn new(year: u32, month: u32, day: u32) -> Result<Date, String> {
+    pub fn new(year: u16, month: u8, day: u8) -> Result<Date, String> {
         if year < 1 {
             return Err("Invalid year. Must be a positive integer.".to_string());
         } else if month < 1 || month > 12 {
             return Err("Invalid month. Must be greater than 0 and less than 12.".to_string());
         }
 
-        let month_days = [31, if is_leap(year) { 29 } else { 28 }, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-        if day < 1 || day > month_days[month as usize - 1] {
-            return Err("Invalid day. Must be greater than 0 and less than {month_days[month as usize-1] - 1}".to_string());
+        let max_days = num_days(year, month);
+        if day < 1 || day > max_days {
+            return Err("Invalid day. Must be greater than 0 and less than {max_days}".to_string());
         }
         Ok(Date { year, month, day })
     }
@@ -38,9 +57,67 @@ impl Date {
     pub fn now() -> Date {
         let current = Local::now();
 
-        let custom = Date::new(current.year() as u32, current.month(), current.day()).unwrap();
-        
+        let custom = Date::new(
+            current.year() as u16,
+            current.month() as u8,
+            current.day() as u8,
+        )
+        .unwrap();
+
         return custom;
+    }
+
+    pub fn get_year(&self) -> u16 {
+        self.year
+    }
+
+    pub fn get_month(&self) -> u8 {
+        self.month
+    }
+
+    pub fn get_day(&self) -> u8 {
+        self.day
+    }
+
+    pub fn set_year(&mut self, year: u16) {
+        self.year = year;
+    }
+
+    pub fn set_month(&mut self, month: u8) {
+        self.month = month;
+    }
+
+    pub fn set_day(&mut self, day: u8) {
+        self.day = day;
+    }
+
+    pub fn add_days(&self, days: u16) -> Date {
+        let mut new_date = Date {
+            year: self.year,
+            month: self.month,
+            day: self.day,
+        };
+
+        let mut days_left = days;
+
+        while days_left > 0 {
+            let diff = num_days(new_date.year, new_date.month) - new_date.day;
+            if days_left <= diff as u16 {
+                new_date.day += days_left as u8;
+                days_left = 0;
+            } else {
+                new_date.day = 1;
+                if new_date.month == 12 {
+                    new_date.month = 1;
+                    new_date.year += 1;
+                } else {
+                    new_date.month += 1;
+                }
+                days_left -= diff as u16;
+            }
+        }
+
+        return new_date;
     }
 }
 
@@ -74,8 +151,18 @@ impl fmt::Display for Date {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         // Array of month names
         let month_names = [
-            "January", "February", "March", "April", "May", "June", "July", "August", "September",
-            "October", "November", "December",
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
         ];
         // Subtract 1 from month to get the index in the month_names array
         let month_name = month_names[self.month as usize - 1];
