74ba272fd28bb293f1e40071fcb233f1908d57a3
Jeffrey Ting <jting@bu.edu>
2024-05-03 22:54:29 -0400

    get_attr function

--- src/entry.rs
+++ src/entry.rs
@@ -3,16 +3,14 @@ use crate::Status;
 use serde::{Deserialize, Serialize};
 use std::fmt;
 
-#[derive(Debug)]
-pub enum SortType {
-    Name,
-    DateAdded,
-    DueDate,
-    Category,
-    Status,
-    Priority,
-    Cost,
-}
+pub const NAME: &str = "name";
+pub const DESCRIPTION: &str = "description";
+pub const DATE_ADDED: &str = "date_added";
+pub const DUE_DATE: &str = "due_date";
+pub const CATEGORY: &str = "category";
+pub const STATUS: &str = "status";
+pub const PRIORITY: &str = "priority";
+pub const COST: &str = "cost";
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Entry {
@@ -79,45 +77,19 @@ impl Entry {
     pub fn get_cost(&self) -> &f32 {
         &self.cost
     }
-}
 
-pub fn sort_by_name(arr: &mut Vec<Entry>) {
-    arr.sort_by(|a, b| a.name.cmp(&b.name));
-}
-
-pub fn sort_by_date_added(arr: &mut Vec<Entry>) {
-    arr.sort_by(|a, b| a.date_added.cmp(&b.date_added))
-}
-
-pub fn sort_by_due_date(arr: &mut Vec<Entry>) {
-    arr.sort_by(|a, b| a.due_date.cmp(&b.due_date))
-}
-
-pub fn sort_by_category(arr: &mut Vec<Entry>) {
-    arr.sort_by(|a, b| a.category.cmp(&b.category));
-}
-
-pub fn sort_by_status(arr: &mut Vec<Entry>) {
-    arr.sort_by(|a, b| a.status.cmp(&b.status));
-}
-
-pub fn sort_by_priority(arr: &mut Vec<Entry>) {
-    arr.sort_by(|a, b| a.priority.cmp(&b.priority));
-}
-
-pub fn sort_by_cost(arr: &mut Vec<Entry>) {
-    arr.sort_by(|a, b| a.cost.partial_cmp(&b.cost).unwrap());
-}
-
-pub fn sort(arr: &mut Vec<Entry>, sort_type: SortType) {
-    match sort_type {
-        SortType::Name => sort_by_name(arr),
-        SortType::DateAdded => sort_by_date_added(arr),
-        SortType::DueDate => sort_by_due_date(arr),
-        SortType::Category => sort_by_category(arr),
-        SortType::Status => sort_by_status(arr),
-        SortType::Priority => sort_by_priority(arr),
-        SortType::Cost => sort_by_cost(arr),
+    pub fn get_attr(&self, attr: &str) -> String {
+        match attr {
+            NAME => self.name.clone(),
+            DESCRIPTION => self.description.clone(),
+            DATE_ADDED => self.date_added.to_string(),
+            DUE_DATE => self.due_date.to_string(),
+            CATEGORY => self.category.clone(),
+            STATUS => self.status.to_string(),
+            PRIORITY => self.priority.to_string(),
+            COST => self.cost.to_string(),
+            _ => String::new(),
+        }
     }
 }
 
@@ -128,8 +100,8 @@ impl fmt::Display for Entry {
             "{} | {} | {} | {}",
             self.name,
             self.category,
-            self.due_date.to_colored_string(),
-            self.status.to_colored_string()
+            self.due_date.to_string(),
+            self.status.to_string()
         )
     }
 }
